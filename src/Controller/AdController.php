d90059bcfca12cd92f980b7cc17f81f6b2e12018
Layeche Torki <layeche.torki@gmail.com>
2021-12-13 20:01:41 +0100

    start of forms

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -3,11 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Ad;
+use App\Form\AdType;
 use App\Repository\AdRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
 
 class AdController extends AbstractController
 {
@@ -35,7 +36,15 @@ class AdController extends AbstractController
      */
     public function create()
     {
-        return $this->render('ad/new.html.twig');
+
+        $ad = new Ad();
+
+        $form = $this->createForm(AdType::class, $ad);
+
+
+        return $this->render('ad/new.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 
     /**
