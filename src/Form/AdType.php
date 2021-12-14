d7bd3be7164f3dd6296664c656ffb053fb41ee4e
Layeche Torki <layeche.torki@gmail.com>
2021-12-14 11:18:23 +0100

    basic configuration for fields

--- src/Form/AdType.php
+++ src/Form/AdType.php
@@ -4,22 +4,44 @@ namespace App\Form;
 
 use App\Entity\Ad;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 
 class AdType extends AbstractType
 {
+
+
+    /**
+     * Permet d'avoir la configuration de base pour un champ ! 
+     *
+     * @param [string] $label
+     * @param [string] $placeholder
+     * @return array
+     */
+    private function getGonfiguration($label, $placeholder)
+    {
+        return [
+            'label' => $label,
+            'attr' => [
+                'placeholder' => $placeholder
+            ]
+        ];
+    }
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('slug')
-            ->add('price')
-            ->add('introduction')
-            ->add('content')
-            ->add('coverImage')
-            ->add('rooms')
-        ;
+            ->add('title', TextType::class, $this->getGonfiguration("Titre", "Tapez un titre pour votre annonce"))
+            ->add('slug', TextType::class, $this->getGonfiguration("Votre url", "Tapez votre adress web"))
+            ->add('price', MoneyType::class, $this->getGonfiguration("Prix par nuit", "Le prix que vous voulez"))
+            ->add('introduction', TextType::class, $this->getGonfiguration("Introduction", "Donnez une description pour votre annonce"))
+            ->add('content', TextareaType::class, $this->getGonfiguration("Description détaillée", "Tapez un description qui donne vraiment envie de venir chez vous !"))
+            ->add('coverImage', UrlType::class, $this->getGonfiguration("URL de l'image principale", "Donnez l'adresse d'une image qui donne vraiment envie"))
+            ->add('rooms', IntegerType::class, $this->getGonfiguration("Nombre de chambres", "Le nombre de chambre disponibles"));
     }
 
     public function configureOptions(OptionsResolver $resolver): void
