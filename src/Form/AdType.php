183f5a42ed607f3bf46b5cf9817935a13d9f8668
Layeche Torki <layeche.torki@gmail.com>
2021-12-16 16:37:01 +0100

    JavaScript for input

--- src/Form/AdType.php
+++ src/Form/AdType.php
@@ -81,7 +81,9 @@ class AdType extends AbstractType
                 CollectionType::class,
                 [
                     'entry_type' => ImageType::class,
-                    'allow_add' => true
+                    'allow_add' => true,
+                    'allow_delete' => true
+
                 ]
             );
     }
