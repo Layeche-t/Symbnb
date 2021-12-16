872dd34fddd5a60a6c81d3e47cd0a39c9ea40434
Layeche Torki <layeche.torki@gmail.com>
2021-12-16 15:03:00 +0100

    add field validations

--- src/Entity/Ad.php
+++ src/Entity/Ad.php
@@ -7,6 +7,7 @@ use App\Repository\AdRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=AdRepository::class)
@@ -23,6 +24,7 @@ class Ad
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Length(min=5, max=255, minMessage="Le titre doit faire plus de 10 caractères !", maxMessage="")
      */
     private $title;
 
