240559067f9b249aa7f5a9005f293aac0290e8bc
Layeche Torki <layeche.torki@gmail.com>
2021-12-20 10:23:47 +0100

    logout

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -8,11 +8,23 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AccountController extends AbstractController
 {
-    /**
+    /** 
+     * Pour ce  connecter 
      * @Route("/login", name="account_login")
      */
     public function login()
     {
         return $this->render('account/login.html.twig');
+    }
+
+    /**
+     * Pour ce déconnecter 
+     * @Route("/logout", name="account_logout")
+     * 
+     * @return void
+     */
+
+    public function logout()
+    {
     }
 }
