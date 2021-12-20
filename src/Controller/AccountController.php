11ba6d6dec1c8cf8ca1ebd49e1cfe2d86ad636c0
Layeche Torki <layeche.torki@gmail.com>
2021-12-20 10:38:45 +0100

    error management

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AccountController extends AbstractController
 {
@@ -12,9 +13,15 @@ class AccountController extends AbstractController
      * Pour ce  connecter 
      * @Route("/login", name="account_login")
      */
-    public function login()
+    public function login(AuthenticationUtils $utils)
     {
-        return $this->render('account/login.html.twig');
+        $error = $utils->getLastAuthenticationError();
+        $usernme = $utils->getLastUsername();
+
+        return $this->render('account/login.html.twig', [
+            'hasError' => $error !== null,
+            'username' => $usernme
+        ]);
     }
 
     /**
