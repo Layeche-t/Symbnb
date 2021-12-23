b9e167b37ada85f3c741aa835cbab872a1b001db
Layeche Torki <layeche.torki@gmail.com>
2021-12-23 15:08:03 +0100

    new style for the form

--- src/Form/BookingType.php
+++ src/Form/BookingType.php
@@ -13,8 +13,8 @@ class BookingType extends ApplicationType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('startDate', DateType::class, $this->getGonfiguration("Date d'arrivée", "La date à laquelle vous comptez arriver"))
-            ->add('endDate', DateType::class, $this->getGonfiguration("Date de départ", "La date à laquelle vous quittez les lieux"));
+            ->add('startDate', DateType::class, $this->getGonfiguration("Date d'arrivée", "La date à laquelle vous comptez arriver", ["widget" => "single_text"]))
+            ->add('endDate', DateType::class, $this->getGonfiguration("Date de départ", "La date à laquelle vous quittez les lieux", ["widget" => "single_text"]));
     }
 
     public function configureOptions(OptionsResolver $resolver): void
