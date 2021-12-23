66c07b279d47c1a7fbb0e385f976b9df9985ed4d
Layeche Torki <layeche.torki@gmail.com>
2021-12-23 15:04:57 +0100

    new form for booking

---
+++ src/Form/BookingType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Booking;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BookingType extends ApplicationType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('startDate', DateType::class, $this->getGonfiguration("Date d'arrivée", "La date à laquelle vous comptez arriver"))
+            ->add('endDate', DateType::class, $this->getGonfiguration("Date de départ", "La date à laquelle vous quittez les lieux"));
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Booking::class,
+        ]);
+    }
+}
