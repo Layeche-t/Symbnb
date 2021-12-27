aa4e2620db3678ff95e3cf4b45310ea4941fc0c6
Layeche Torki <layeche.torki@gmail.com>
2021-12-27 11:01:49 +0100

    booking registration

--- src/Controller/BookingController.php
+++ src/Controller/BookingController.php
@@ -5,10 +5,10 @@ namespace App\Controller;
 use App\Entity\Ad;
 use App\Entity\Booking;
 use App\Form\BookingType;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class BookingController extends AbstractController
 {
@@ -16,10 +16,26 @@ class BookingController extends AbstractController
      * @Route("/ads/{slug}/book", name="booking_create")
      * @IsGranted("ROLE_USER")
      */
-    public function book(Ad $ad)
+    public function book(Ad $ad, Request $request)
     {
         $booking = new Booking();
         $form = $this->createForm(BookingType::class, $booking);
+
+        $form->handleRequest($request);
+
+        $manager = $this->getDoctrine()->getManager();
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $user = $this->getUser();
+            $booking->setBooker($user)
+                ->setAd($ad);
+
+            $manager->persist($booking);
+            $manager->flush();
+
+            return $this->redirectToRoute('booking_success', ['id' => $booking->getId()]);
+        }
 
         return $this->render('booking/book.html.twig', [
             'ad' => $ad,
