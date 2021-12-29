d47e7c3e2adf812e1f3f1b39f8133a50cee41b66
Layeche Torki <layeche.torki@gmail.com>
2021-12-29 20:33:51 +0100

    change of date format

--- src/Form/BookingType.php
+++ src/Form/BookingType.php
@@ -4,18 +4,18 @@ namespace App\Form;
 
 use App\Entity\Booking;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class BookingType extends ApplicationType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('startDate', DateType::class, $this->getGonfiguration("Date d'arrivée", "La date à laquelle vous comptez arriver"))
-            ->add('endDate', DateType::class, $this->getGonfiguration("Date de départ", "La date à laquelle vous quittez les lieux"))
+            ->add('startDate', TextType::class, $this->getGonfiguration("Date d'arrivée", "La date à laquelle vous comptez arriver"))
+            ->add('endDate', TextType::class, $this->getGonfiguration("Date de départ", "La date à laquelle vous quittez les lieux"))
             ->add('comment', TextareaType::class, $this->getGonfiguration(false, "Ajouter un commentaire"));
     }
 
